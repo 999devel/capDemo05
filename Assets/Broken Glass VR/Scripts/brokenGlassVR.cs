f4fb72a2aac5d8aa98cb93450e37c191152ea4a2
hojun999 <148923751+999devel@users.noreply.github.com>
2023-11-07 05:11:34 +0900

    10주차_UI 및 유리 장식 추가
    10주차_UI 및 유리 장식 추가

---
+++ Assets/Broken Glass VR/Scripts/brokenGlassVR.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brokenGlassVR : MonoBehaviour {
+	private Material _glassMat;
+	private bool _cracked;
+	[SerializeField]private float _distortion_before;
+	private float _distortion;
+	public float _repairSpeed;
+
+	void Start () {
+		_glassMat = GetComponent<MeshRenderer> ().material;
+	}
+	
+
+	void Update () {
+		_glassMat.SetFloat ("_BumpAmt", _distortion);
+		if (_cracked) {
+		
+			_distortion = Mathf.MoveTowards (_distortion, 0f, _repairSpeed * Time.unscaledDeltaTime);
+			if (_distortion == 0f) {
+			
+				_cracked = false;
+			
+			}
+		}
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+			GlassShatter();
+
+		}
+
+	}
+
+	public void GlassShatter(){
+	
+		_distortion = _distortion_before;
+		_cracked = true;
+	
+	}
+
+}
